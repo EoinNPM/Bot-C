6e5f69374eb235bc0e135656d0418082e5734e52
EoinNPM <eoinnpm@gmail.com>
2024-01-14 14:28:27 +0000

    Add files via upload

--- cogs/phase_shifts.py
+++ cogs/phase_shifts.py
@@ -44,12 +44,11 @@ class PhaseShifts(commands.Cog, name='Phase Shifts'):
                 for player_id, channel_id in cabin_ownership_dict.items():
                     player_id = int(player_id)
 
-                    if channel_id in available_channel_ids:
+                    if (channel_id in available_channel_ids) and (player_id in town_square_member_ids):
                         available_channel_ids.remove(channel_id)
 
-                        if player_id in town_square_member_ids:
-                            movements[player_id] = channel_id
-                            town_square_member_ids.remove(player_id)
+                        movements[player_id] = channel_id
+                        town_square_member_ids.remove(player_id)
 
             if len(available_channel_ids) >= len(town_square_member_ids):
                 available_channel_ids = list(available_channel_ids)
